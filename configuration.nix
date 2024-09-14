0063938664a4903685fb0bfcc6edffd20f361022
Hortasha <eidemartin_303@hotmail.com>
2024-09-14 12:36:43 +0200

    tweaks

--- configuration.nix
+++ configuration.nix
@@ -5,5 +5,5 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
   ];
 
-   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
